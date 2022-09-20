75e3d86e7a9c027b214734b8c65d5255b7660e21
jamesonjolley <jameson.m.jolley@gmail.com>
2022-09-19 20:25:30 -0600

    week2 stuff and notes

---
+++ week2/js/nots.js
@@ -0,0 +1,45 @@
+let b = 2;
+{b = 4;
+	console.log(b);
+}
+
+console.log(b);
+// this did't wor as expeted idk why
+
+//
+//Naming Constants & Variables
+//
+//Constant and variable names can start with any upper or lower-case letter, an underscore, _, or dollar character, $. 
+//Thy can also contain numbers, but cannot start with them.
+
+//	Here are some valid examples:
+//	$name
+//	_answer
+//	firstName
+//	last_name
+//	address_line1
+//
+
+// strings
+//
+// you can construrct strings in difret ways
+//
+Astring = new String("hello")
+$string = 'this is a string'
+
+console.log(Astring);
+console.log($string);
+
+
+// you can't access properties using dictionary like notation instead of. Notation
+
+console.log(Astring["lenght"]);
+console.log(Astring.length);
+
+
+let new_num = 0xAFF
+console.log(new_num);
+
+//Quiz Ninja Project
+
+
